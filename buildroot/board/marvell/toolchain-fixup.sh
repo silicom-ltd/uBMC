faab14bebfb58c0b9b753446140b8926f46f2c0e
Jason Zhang <jasonz@silicom.co.il>
2020-05-26 09:58:00 +0800

    fix  lack of nsl lib

--- buildroot/board/marvell/toolchain-fixup.sh
+++ buildroot/board/marvell/toolchain-fixup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 STAGING_DIR=$1
 ln -snf ld-2.27.so ${STAGING_DIR}/lib/ld-linux-aarch64.so.1
+#fix :can not link libnsl when build is_console_manage
+ln -snf libnsl-2.27.so ${STAGING_DIR}/lib/libnsl.so
